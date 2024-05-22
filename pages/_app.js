f76291b1399500e22205adbab3b7493732c4b811
Your Name <khusbu.mallick@gmail.com>
2024-05-22 14:14:42 +0530

    fixes ids

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import { Auth0Provider } from "@auth0/auth0-react"
 export default function App({ Component, pageProps }) {
   return  (
     <Auth0Provider
-    domain={process.env.AUTH0_DOMAIN}
-    clientId={process.env.AUTH0_CLIENT_ID}
-    authorizationParams={{ redirect_uri: process.env.AUTH0_REDIRECT_URI }}
+    domain="dev-aeb2d5hyznx21v1u.us.auth0.com"
+    clientId="tEup7V7xqSwWjAEJPXvlGLbhG7yr5jxi"
+    authorizationParams={{ redirect_uri: "https://joyful-tiramisu-a0713b.netlify.app/callback" }}
     >
   <Component {...pageProps} />
   </Auth0Provider>
