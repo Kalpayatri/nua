4ad9d48a5a4b3d59128406f1438233c4012b9ecf
Your Name <khusbu.mallick@gmail.com>
2024-05-22 14:17:40 +0530

    fixes ids

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
     domain="dev-aeb2d5hyznx21v1u.us.auth0.com"
     clientId="tEup7V7xqSwWjAEJPXvlGLbhG7yr5jxi"
     authorizationParams={{ redirect_uri: "https://joyful-tiramisu-a0713b.netlify.app/callback" }}
+    
     >
   <Component {...pageProps} />
   </Auth0Provider>
